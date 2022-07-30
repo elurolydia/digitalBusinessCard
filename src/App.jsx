d841aa1ddf4444156f201c57c511166188738496
Lydia Eluro <elurolydia@gmail.com>
2022-07-29 23:29:58 -0500

    added About

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Info from './components/Info'
+import About from './components/About'
 
 
 export default function App () {
   return (
     <div className="page">
       <Info />
+      <About />
     </div>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+export default function About () {
+    return (
+        <div className="aboutdiv">
+            <h2>About</h2>
+            <p>I am a frontend developer with a passion for solving problems. 
+                I also love to learn new things and keep up with best practices. Coding is my hubby.
+            </p>
+        </div>
+    )
+}
