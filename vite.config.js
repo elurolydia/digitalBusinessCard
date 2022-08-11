8e003846e7d9c0b3d9e9f049e031810866446ecf
Lydia Eluro <elurolydia@gmail.com>
2022-08-10 21:47:21 -0500

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/digitalBusinessCard/",
+  base: '/digitalBusinessCard/',
   plugins: [react()]
 })
